03db66add39c31111ebf79910fedbd9362c8d9ac
OlivierBC <202330063@edu.clg.qc.ca>
2026-03-02 03:14:57 -0500

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Raylib CS");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+    Raylib.DrawText("Raylib Base Project", 20, 20, 20, Color.White);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
